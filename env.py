abc6b70a95f51db7746b99bfcc1da86867fb13cf
Suroo <SDHUGHES1998@GMAIL.COM>
2022-05-30 10:01:37 +0000

    added database to heroku and migrated

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://hatqvfztzxfowa:4a32b01ab80b401213f0e1692934bb57164c5e88807a7f962282c05b28b8f094@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/d2hk9r00koh4pc"
+os.environ["SECRET_KEY"] = "admin"
